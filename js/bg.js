44bccbd666e46448b3b5a335a8cb7704cefae0d7
kree <glol23m@163.com>
2018-01-10 12:46:22 +0800

    Site updated: 2018-01-10 12:46:20

--- js/bg.js
+++ js/bg.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() {
   window.pattern = Trianglify({
-    width: window.innerWidth,
-    height: window.innerHeight
+    width: window.innerWidth + 100,
+    height: window.innerHeight + 200
   });
   //document.body.appendChild(pattern.canvas())
   //$("body").css("background-image",pattern.svg());
